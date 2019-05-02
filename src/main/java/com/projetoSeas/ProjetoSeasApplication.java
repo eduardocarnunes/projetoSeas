2f732b6825fd383344a6695de4306944d3eba352
Eduardo Carvalho Nunes <eduardocarvalho-1992@hotmail.com>
2019-05-02 22:21:03 +0100

    Projeto Criado

---
+++ src/main/java/com/projetoSeas/ProjetoSeasApplication.java
@@ -0,0 +1,13 @@
+package com.projetoSeas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSeasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSeasApplication.class, args);
+	}
+
+}
